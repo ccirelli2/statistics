51bd7d757da7ddcea7f7d54042c55ce1ecc0c1ab
cc2 <ccirelli2@gmail.com>
2020-04-13 16:19:27 -0400

    initial commit

---
+++ poisson_regression.R
@@ -0,0 +1,8 @@
+## DOCUMENTATION _______________________________________________
+'	Desc:	Poisson regression tutorial. 
+	Ref:	https://www.dataquest.io/blog/tutorial-poisson-regression-in-r/
+'
+
+
+
+
