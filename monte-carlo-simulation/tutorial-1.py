adbd6e1e447682cb82b8fb7db5da6dd2f834a591
ccirelli2 <ccirelli2@gmail.com>
2022-02-13 14:39:13 -0500

    Initial commit.  Monte-Carlo-Simulation basic tutorial.

---
+++ monte-carlo-simulation/tutorial-1.py
@@ -0,0 +1,13 @@
+"""
+Monte-Carlo-Simulation-Tutorial
+Reference: https://urldefense.com/v3/__https://link.medium.com/ZlNy152Qunb__;!!N0ucV8FIT4MI!3mdzbdQx0CIoLYDGNM3GJEob9wH0DA18RPRn8_l8ET8_7Vq1BMLEv3iRoeHXql7tSdNk$
+
+Description:
+A Monte Carlo simulation is a type of computational algorithm that estimates the
+    - probability of occurrence of an undeterminable event due to the involvement of random variables ***.
+    - The algorithm relies on repeated random sampling in an attempt to determine the probability.
+    - This means simulating an event with random inputs a large number of times to obtain your estimation.
+    - You can determine other factors as well, and we will see that in the example.
+"""
+
+
