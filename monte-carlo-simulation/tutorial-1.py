424c323328d839934ea75eeed7d4e02321f9a87a
ccirelli2 <ccirelli2@gmail.com>
2022-03-02 09:31:28 -0500

    Initial commit.  Monte-Carlo-Simulation basic tutorial.

--- monte-carlo-simulation/tutorial-1.py
+++ monte-carlo-simulation/tutorial-1.py
@@ -1,13 +1,19 @@
 """
 Monte-Carlo-Simulation-Tutorial
-Reference: https://urldefense.com/v3/__https://link.medium.com/ZlNy152Qunb__;!!N0ucV8FIT4MI!3mdzbdQx0CIoLYDGNM3GJEob9wH0DA18RPRn8_l8ET8_7Vq1BMLEv3iRoeHXql7tSdNk$
-
 Description:
 A Monte Carlo simulation is a type of computational algorithm that estimates the
     - probability of occurrence of an undeterminable event due to the involvement of random variables ***.
     - The algorithm relies on repeated random sampling in an attempt to determine the probability.
     - This means simulating an event with random inputs a large number of times to obtain your estimation.
     - You can determine other factors as well, and we will see that in the example.
+
+References:
+    https://urldefense.com/v3/__https://link.medium.com/ZlNy152Qunb__;!!N0ucV8FIT4MI!3mdzbdQx0CIoLYDGNM3GJEob9wH0DA18RPRn8_l8ET8_7Vq1BMLEv3iRoeHXql7tSdNk$
+    https://towardsdatascience.com/monte-carlo-simulations-with-python-part-1-f5627b7d60b0
+    https://towardsdatascience.com/monte-carlo-simulation-and-variants-with-python-43e3e7c59e1f
+    https://quant.stackexchange.com/questions/45560/python-libraries-for-monte-carlo-simulations
+    https://pbpython.com/monte-carlo.html
 """
 
+# Import Python Packages
 
